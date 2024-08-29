4f36425136f38ec13c7c8ba422f9abf8468ff4e9
pankajmanral <pankajmanral0208@gmail.com>
2024-08-29 22:10:13 +0530

    towSum

---
+++ twoSum.py
@@ -0,0 +1,34 @@
+nums = [2,7,11,15]
+target = 9
+
+# First method
+# Time complexity => o(n) ^ 2
+
+# class Solution():
+#     def twoSum(self,nums:int,target:int) -> list[int]:
+#         for i in range(len(nums)):
+#             for j in range(i+1,len(nums)):
+#                 if nums[i] + nums[j] == target:
+#                     return [i,j]
+#         return []
+
+# Second method
+# Time complexity => o(n)
+
+class Solution():
+    def twoSum(self,nums:int,target:int) -> list[int]:
+        nums.sort()
+        start = 0
+        end = len(nums) - 1
+        while start < end:
+            result = nums[start] + nums[end]
+            if result == target:
+                return [start,end]
+            elif result > target:
+                end -= 1
+            else:
+                start += 1
+
+sol = Solution()
+result = sol.twoSum(nums,target)
+print(result)
